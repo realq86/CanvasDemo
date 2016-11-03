b45f08dcd3db90767c46d15f037bce60b89a4a7d
Ruchit Mehta <dharabavishi@Ruchits-MacBook-Pro.local>
2016-11-02 19:28:46 -0700

    Milestone 2

--- CanvasDemo/ViewController.swift
+++ CanvasDemo/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var startPoint : CGPoint!
+    @IBOutlet weak var trayView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func onTtayPanGesture(_ sender: UIPanGestureRecognizer) {
+        
+        
+        let point = sender.location(in: view)
+        if sender.state == .began{
+            print("Gesture began at: \(point)")
+            startPoint = trayView.center
+            
+            
+        }else if sender.state == .changed{
+            print("Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: startPoint.x, y: startPoint.y+point.y)
+        }else if sender.state == .ended{
+            print("Gesture ended at: \(point)")
+            trayView.center = CGPoint(x: startPoint.x, y: startPoint.y+point.y)
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
