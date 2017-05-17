2d472c0800bdc71460e1d00de9708862823ae93e
Michael <realq86@gmail.com>
2017-05-17 11:36:21 -0700

    Finishing up Canvas demo

--- CanvasDemo/ViewController.swift
+++ CanvasDemo/ViewController.swift
@@ -8,20 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
     var startPoint : CGPoint!
     @IBOutlet weak var trayView: UIView!
+    @IBOutlet weak var arrayImageView: UIImageView!
     
     var newlyCreatedImageView:UIImageView! {
         didSet {
             newlyCreatedImageView.isUserInteractionEnabled = true
-            let gestureRec = UIPanGestureRecognizer(target: self, action: #selector(onNewImageView(_:)))
-            newlyCreatedImageView.addGestureRecognizer(gestureRec)
+            let panGestureRec = UIPanGestureRecognizer(target: self, action: #selector(panNewImageView(_:)))
+            panGestureRec.delegate = self
+            newlyCreatedImageView.addGestureRecognizer(panGestureRec)
+            
+            let pinchGestureRec = UIPinchGestureRecognizer(target: self, action: #selector(pinchImageView(_:)))
+            pinchGestureRec.delegate = self
+            
+            newlyCreatedImageView.addGestureRecognizer(pinchGestureRec)
         }
     }
     
     var startPointOfImage: CGPoint!
+    
+    var sizeOfImage:CGSize!
+    
+    var trayIsOpen:Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,36 +41,56 @@ class ViewController: UIViewController {
 
     @IBAction func onTtayPanGesture(_ sender: UIPanGestureRecognizer) {
         
-        
+        print("Tray Pan")
         let point = sender.location(in: view)
         let translation = sender.translation(in: view)
         let velocity = sender.velocity(in: view)
         
-
         if sender.state == .began{
-            print("Gesture began at: \(point)")
+            
+            print("Tray Gesture began at: \(point)")
             startPoint = trayView.center
-            
-            
+
+
         }else if sender.state == .changed{
-            print("Gesture changed at: \(point)")
-           trayView.center = CGPoint(x: startPoint.x, y: startPoint.y+translation.y)
-        }else if sender.state == .ended{
-            print("Gesture ended at: \(point)")
             
-            UIView.animate(withDuration: 0.3, animations: {
-                 self.trayView.center = CGPoint(x: self.startPoint.x, y: self.startPoint.y+translation.y)
-                if velocity.y > 0.0 {
-                    //going down
-                    self.trayView.frame.origin.y = self.view.frame.size.height - 30
-                }
-                else {
-                    //going up
-                    
-                    self.trayView.frame.origin.y = self.view.frame.size.height-self.trayView.frame.size.height
-                }
+            print("Tray Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: startPoint.x, y: startPoint.y+translation.y)
+            
+            let angleDegree = Float(translation.y).truncatingRemainder(dividingBy: 180)
+            
+            self.arrayImageView.transform = CGAffineTransform(rotationAngle: (CGFloat(angleDegree * .pi/180)))
+            
+        }else if sender.state == .ended{
+            
+            print("Tray Gesture ended at: \(point)")
+            
+
+            UIView.animate(withDuration: 0.3,
+                           delay: 0,
+                           usingSpringWithDamping: 0.4,
+                           initialSpringVelocity: 0.1,
+                           options: [],
+                           animations: {
+                            self.trayView.center = CGPoint(x: self.startPoint.x, y: self.startPoint.y+translation.y)
+                            if velocity.y > 0.0 {
+                                //going down
+                                self.trayView.frame.origin.y = self.view.frame.size.height - 30
+                                self.trayIsOpen = false
+                            }
+                            else {
+                                //going up
+                                self.trayView.frame.origin.y = self.view.frame.size.height-self.trayView.frame.size.height
+                                self.trayIsOpen = true
+                            }
+            },
+                           completion: { (complete:Bool) in
+                            
             })
-          
+        }
+        else if sender.state == .failed {
+            
+            print("Tray Gesture failed")
         }
         
     }
@@ -69,55 +100,95 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onImageView(_ sender: UIPanGestureRecognizer) {
-        let imageView = sender.view as! UIImageView
+        print("View Pan")
 
         let translation = sender.translation(in: view)
         
-//        let oldImageViewTranslate 
         if sender.state == .began{
-            print("Gesture began at")
+            print("View Gesture began at")
 
             let imageView = sender.view as! UIImageView
             startPointOfImage = imageView.center
             
-            startPointOfImage = imageView.center
             newlyCreatedImageView = UIImageView(image: imageView.image)
             self.view.addSubview(newlyCreatedImageView)
             newlyCreatedImageView.center = imageView.center
             newlyCreatedImageView.center.y += trayView.frame.origin.y
-            
         }else if sender.state == .changed{
-            print("Gesture changed aT")
+            print("View Gesture changed aT")
 
-//            let imageView = sender.view as! UIImageView
             newlyCreatedImageView.center = CGPoint(x: startPointOfImage.x + translation.x, y: startPointOfImage.y + translation.y + trayView.frame.origin.y)
-        
         }else if sender.state == .ended{
-            
-            
 
-            
-//            self.addPanTo(imageView: newlyCreatedImageView)
-            
         }
     }
     
     
-//    func addPanTo(imageView:UIImageView) {
-//        let gestureRec = UIPanGestureRecognizer(target: self, action: #selector(onNewImageView(_:)))
+//    func addPanTo(_ imageView:UIImageView) {
+//        
+//        let gestureRec = UIPanGestureRecognizer(target: self, action: #selector(panNewImageView(_:)))
 //        imageView.addGestureRecognizer(gestureRec)
 //        imageView.isUserInteractionEnabled = true
+//    }
+//    
+//    func addPinchTo(_ imageView:UIImageView) {
 //        
-//
+//        let gesturRec = UIPinchGestureRecognizer(target: self, action: #selector(pinchImageView(_:)))
+//        
+//        imageView.addGestureRecognizer(gesturRec)
+//        imageView.isUserInteractionEnabled = true
 //    }
     
-    func onNewImageView(_ sender: UIPanGestureRecognizer) {
-        
-        if sender.state == .began {
-            print("ON NEW IMAGE VIEW")
- 
+    func pinchImageView (_ sender:UIPinchGestureRecognizer) {
+    
+        let view = sender.view
+//        let originalScale = 
+        switch sender.state {
+        case .began:
+            print("Begin pinch")
+            
+            break
+        case .changed:
+            
+            view?.transform = (view?.transform.scaledBy(x: sender.scale, y: sender.scale))!
+            sender.scale = 1
+            print("Pinch changed")
+            break
+        default:
+            break
         }
+    }
+    
+    func panNewImageView(_ sender: UIPanGestureRecognizer) {
         
+        let view = sender.view!
+        
+        switch sender.state {
+        case .began:
+            self.sizeOfImage = sender.view?.bounds.size
+
+            UIView.animate(withDuration: 0.2, animations: {
+                view.transform = view.transform.scaledBy(x: 2, y: 2)
+            })
+        case .changed:
+            var delta = sender.translation(in: self.view)
+            delta.x += view.center.x
+            delta.y += view.center.y
+            
+            view.center = delta
+            
+            sender.setTranslation(CGPoint.zero, in: self.view)
+        case .ended:
+            UIView.animate(withDuration: 0.2, animations: {
+                view.transform = view.transform.scaledBy(x: 0.5, y: 0.5)
+
+            })
+        default: break
+        }
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
 
